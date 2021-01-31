ffa48ef6566e28ec7d6b15302a676dbb27bbde69
Rubén Álvarez González <rubenag83@gmail.com>
2021-01-31 20:22:27 +0100

    Se implementa una lista blanca

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { getFilesWithKeyword } from './utils/getFilesWithKeyword';
 import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
 
 
+let whitelist = ['http://localhost:3000']
 
 const app: Express = express();
 
@@ -24,11 +25,18 @@ if (process.env.NODE_ENV === 'development' || config.NODE_ENV === 'development')
 }
 
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    if(!origin) return callback(null, true);
+    if(whitelist.indexOf(origin) === -1){
+      var message = "The CORS policy for this origin doesn't allow access from the particular origin.";
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Handle security and origin in production
 if (process.env.NODE_ENV === 'production' || config.NODE_ENV === 'production') {
